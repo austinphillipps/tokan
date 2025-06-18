cb6de5fc8c72dba07063b08d339eb931147857d6
austinphillipps <59981068+austinphillipps@users.noreply.github.com>
2025-06-18 12:14:39 -0400

    Add DashboardWidgetProvider and integrate provider

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import 'package:provider/provider.dart';
 
 // Vos providers existants
 import 'core/providers/plugin_provider.dart';
+import 'features/dashboard/providers/dashboard_widget_provider.dart';
 
 import 'features/auth/services/auth_service.dart';
 import 'features/auth/views/login_screen.dart';
@@ -199,6 +200,7 @@ Future<void> main() async {
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => PluginProvider()),
+        ChangeNotifierProvider(create: (_) => DashboardWidgetProvider()),
         // Les providers dépendant de l'utilisateur seront instanciés
         // plus tard, une fois authentifié, pour éviter les erreurs.
       ],
